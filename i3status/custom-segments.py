5f0173673a33483aa67c8ac7d5345e3de81e9613
KoffeinFlummi <koffeinflummi@gmail.com>
2015-06-20 19:56:57 +0000

    Differentiate between PC and laptop ...
    ... in custom segment script

--- i3status/custom-segments.py
+++ i3status/custom-segments.py
@@ -4,15 +4,19 @@ import os
 import sys
 import math
 import locale
+import re
 import subprocess as sp
 
 
 def get(cmd):
     out = sp.check_output(cmd.split())
     out = str(out, "utf-8")
-    if out[-1] == "\n":
-        return out[:-1]
-    return out
+    try:
+        if out[-1] == "\n":
+            return out[:-1]
+        return out
+    except:
+        return ""
 
 
 def main():
@@ -22,6 +26,7 @@ def main():
     argvb = [bytes(arg, encoding) for arg in argv]
     argv = [str(arg, "utf-8") for arg in argvb]
 
+    # Find insertion point for new segments
     original = " ".join(argv[1:])
     try:
         start = original.index("[") + 1
@@ -29,16 +34,24 @@ def main():
     except:
         return original
 
+    # Remove ethernet segment on laptop, and battery segment on PC
+    if "laptop" in get("hostname"):
+        original = re.sub(r",\{\"name\":\"ethernet\".*?\}", "", original)
+    else:
+        original = re.sub(r",\{\"name\":\"battery\".*?\}", "", original)
+
+    # Construct additional segments
     segments = []
+    if "laptop" in get("hostname"):
+        # Backlight
+        backlight = math.floor(float(get("xbacklight -get")))
+        segments.append(" {:03}%".format(backlight))
 
-    # Backlight
-    backlight = math.floor(float(get("xbacklight -get")))
-    segments.append(" {:03}%".format(backlight))
+        # Kbdlight
+        kbdlight = math.floor(float(get("kbdlight get")) / 255 * 100)
+        segments.append(" {:03}%".format(kbdlight))
 
-    # Kbdlight
-    kbdlight = math.floor(float(get("kbdlight get")) / 255 * 100)
-    segments.append(" {:03}%".format(kbdlight))
-
+    # Insert additional segments
     output = original[:start]
     for i, s in enumerate(segments):
         output += '{' + '"name":"custom_{}","full_text":"{}"'.format(i, s) + '},'
