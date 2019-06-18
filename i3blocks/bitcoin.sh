8a45c6598117c179026e42e286bf9a1ca1b89f2d
KoffeinFlummi <koffeinflummi@protonmail.com>
2019-06-18 19:47:43 +0200

    Update i3 status

--- i3blocks/bitcoin.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-btc=$(curl eur.rate.sx/btc | grep avg | egrep -o "[0-9]+\.[0-9]*" | head -1)
-ltc=$(curl eur.rate.sx/ltc | grep avg | egrep -o "[0-9]+\.[0-9]*" | head -1)
-
-echo " $btc€ L $ltc€"
--- i3blocks/updates.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-updates=$(($(checkupdates | wc -l) + $(cower -u | wc -l)))
-[[ "$updates" = "0" ]] && exit 0
-
-echo " $updates"
-echo " $updates"
-echo "#fb4934"
